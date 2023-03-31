d1607e114f25ae8a07f88d94d332d00267a68a7d
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-03-31 22:57:29 +0700

    TinhDienTich

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    class SuDungToanTu
+    {
+        static void Main(string[] args)
+        {
+            float width, height;
+
+            Console.Write("Enter the width: ");
+            while (!float.TryParse(Console.ReadLine(), out width))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.Write("Enter the width: ");
+            }
+
+            Console.Write("Enter the height: ");
+            while (!float.TryParse(Console.ReadLine(), out height))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.Write("Enter the height: ");
+            }
+
+            float area = width * height;
+            Console.WriteLine("The area is: " + area);
+        }
+    }
+}
+
